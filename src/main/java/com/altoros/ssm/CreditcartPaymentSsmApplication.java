a3ed6cb83d4e4ebca8d3513c987ec399a0626c48
kmarabet <kamilmarabet@gmail.com>
2020-01-16 12:23:14 +0300

    Initial commit

---
+++ src/main/java/com/altoros/ssm/CreditcartPaymentSsmApplication.java
@@ -0,0 +1,13 @@
+package com.altoros.ssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcartPaymentSsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcartPaymentSsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altoros/ssm/CreditcartPaymentSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altoros.ssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditcartPaymentSsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
